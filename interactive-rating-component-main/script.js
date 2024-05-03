2979105c4ab0d7f42dd18c431a8e72714483afde
Arjunkb2003 <164005389+Arjunkb2003@users.noreply.github.com>
2024-05-03 13:14:16 +0530

    Add files via upload

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,13 @@
+// var submit = document.querySelector(".sub")
+// submit.addEventListener=('onclick',function(){
+//     if(submit.style.color===white )
+//     submit.style.color="orange";
+//     submit.style.backgroundColor="white";
+// })
+
+var one = document.getElementById("one")
+var two = document.getElementById("two")
+var three = document.getElementById("three")
+var four = document.getElementById("four")
+var five = document.getElementById("five")
+
